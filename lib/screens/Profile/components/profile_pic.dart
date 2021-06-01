e6a08f7b381a043a44f0b034fa5d069b6c03fb4b
HubertTjendra22 <huberttjendra22@gmail.com>
2021-06-02 01:00:16 +0700

    Gabungin stepet
    Untuk home dan profile picture yang udah di perbarui

--- lib/screens/Profile/components/profile_pic.dart
+++ lib/screens/Profile/components/profile_pic.dart
@@ -4,9 +4,12 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gas_gameappstore/exceptions/local_files_handling/image_picking_exceptions.dart';
 import 'package:gas_gameappstore/exceptions/local_files_handling/local_file_handling_exception.dart';
 import 'package:gas_gameappstore/services/local_files_access/local_files_access_service.dart';
+import 'package:gas_gameappstore/services/database/user_database_helper.dart';
 import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 import '../provider_models/body_model.dart';
+import '../../../size_config.dart';
+import '../../../constants.dart';
 
 class ProfilePic extends StatelessWidget {
   const ProfilePic({
@@ -19,38 +22,78 @@ class ProfilePic extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => ChosenImage(),
-      child: SizedBox(
-        height: 115,
-        width: 115,
-        child: Stack(
-          fit: StackFit.expand,
-          overflow: Overflow.visible,
-          children: [
-            CircleAvatar(
-              backgroundImage: AssetImage("assets/images/Profile Image.png"),
-            ),
-            Positioned(
-              right: -16,
-              bottom: 0,
-              child: SizedBox(
-                height: 46,
-                width: 46,
-                child: FlatButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50),
-                    side: BorderSide(color: Colors.white),
-                  ),
-                  color: Color(0xFFF5F6F9),
-                  onPressed: () {
-                    getImageFromUser(context, bodyState);
-                  },
-                  child: SvgPicture.asset("assets/icons/Camera Icon.svg"),
-                ),
+      child: SafeArea(
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: getProportionScreenWidth(screenPadding)),
+            child: SizedBox(
+              width: 175,
+              height: 160,
+              child: Consumer<ChosenImage>(
+                builder: (context, bodyState, child) {
+                  return Stack(
+                    children: [
+                      SizedBox(height: getProportionScreenHeight(0)),
+                      GestureDetector(
+                        child: buildDisplayPictureAvatar(context, bodyState),
+                        // onTap: () {
+                        //   getImageFromUser(context, bodyState);
+                        // },
+                      ),
+                       Positioned(
+                          right: 5,
+                          bottom: 5,
+                          child: SizedBox(
+                            height: 55,
+                            width: 55,
+                            child: FlatButton(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(40),
+                              side: BorderSide(color: Colors.white),
+                            ),
+                            color: Color(0xFFF5F6F9),
+                            onPressed: () {
+                              getImageFromUser(context, bodyState);
+                            },
+                            child: SvgPicture.asset("assets/icons/Camera Icon.svg"),
+                          ),
+                        ),
+                      )
+                    ],
+                  );
+                },
               ),
-            )
-          ],
+            ),
+          ),
         ),
       ),
+    );
+  }
+
+  Widget buildDisplayPictureAvatar(
+      BuildContext context, ChosenImage bodyState) {
+    return StreamBuilder(
+      stream: UserDatabaseHelper().currentUserDataStream,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          final error = snapshot.error;
+          Logger().w(error.toString());
+        }
+        ImageProvider backImage;
+        if (bodyState.chosenImage != null) {
+          backImage = MemoryImage(bodyState.chosenImage.readAsBytesSync());
+        } else if (snapshot.hasData && snapshot.data != null) {
+          final String url = snapshot.data.data()[UserDatabaseHelper.USER_PROFILE_PICTURE_KEY];
+          if (url != null) backImage = NetworkImage(url);
+        }
+        return CircleAvatar(
+          radius: SizeConfig.screenWidth * 0.24,
+          backgroundColor: kTextColor.withOpacity(0.5),
+          backgroundImage: backImage ?? null,
+        );
+      },
     );
   }
 
--- lib/screens/Profile/profile_screen.dart
+++ lib/screens/Profile/profile_screen.dart
@@ -1,14 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'components/body.dart';
+import '../../size_config.dart';
 
 class ProfileScreen extends StatelessWidget {
   static String routeName = "/profile";
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Profile"),
+        title: Text(
+            "Profile",
+            style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.w300,
+          ),
+        )
       ),
       body: Body(),
     );
